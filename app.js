4c901539c60b88a195b684c6a15bd2ca5ca89f02
gaurav dhra <dhradhra@gmail.com>
2020-10-13 11:52:24 +0530

    update

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();  
 
-app.use(express.static(path.join(__dirname, '/dist/POS-ADMIN/')));
-
+app.use(express.static(path.join(__dirname, '/dist/POS-Admin/')));
+console.log(path.join(__dirname, '/dist/POS-Admin/'))
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/POS-ADMIN/'));
+  res.sendFile(path.join(__dirname + '/dist/POS-Admin/'));
 }); 
 const port = process.env.PORT || 5000;
 app.set('port', port);
